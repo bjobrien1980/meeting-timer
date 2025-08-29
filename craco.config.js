44fbf7b24ea6a71beb16a1fe6e86716745454a52
bjobrien1980 <bjobrien1980@gmail.com>
2025-08-29 16:34:43 +0800

    Add CRACO configuration for Tailwind CSS processing

---
+++ craco.config.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+};
